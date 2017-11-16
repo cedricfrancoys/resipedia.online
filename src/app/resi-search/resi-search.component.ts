aa2794dd2b6c36ac77d21f26ef4af6f643f0649c
cedricfrancoys <cedricfrancoys@gmail.com>
2017-11-16 13:52:02 +0100

    initial import

---
+++ src/app/resi-search/resi-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'resi-search',
+  templateUrl: './resi-search.component.html',
+  styleUrls: ['./resi-search.component.css']
+})
+export class ResiSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
